ac0497554053007b20b6761f068f2cf255b14d8e
PaulaCCh <pcaballero.ch@gmail.com>
2015-05-02 12:36:55 +0200

    Heroku   autor

--- models/models.js
+++ models/models.js
@@ -14,10 +14,10 @@ var storage= process.env.DATABASE_STORAGE;
 //Cargar Modelo ORM
 var Sequelize = require('sequelize');
 //Usar BBDD SQLite o POstgres
-var sequelize= new Sequelize(DB_name, user, pwd,{dialect:protocol,protocol:protocol,port:port,host:host,storage:storage,omitNull:true}
+var sequelize = new Sequelize(DB_name, user, pwd,{dialect:protocol,protocol:protocol,port:port,host:host,storage:storage,omitNull:true}
 	);
 //Importar la definicion de la tabla de Quiz en quiz.js
-var Quiz = sequelize.import(path.join(_dirname,'quiz'));
+var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 exports.Quiz=Quiz; //Exporta definicion de tabla Quiz
 
 //sequelize.sync() crea e inicializa tabla de preguntas en DB
