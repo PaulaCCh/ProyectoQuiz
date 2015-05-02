9b3c496b8219847fd9fdf874a2275f614daa3af1
PaulaCCh <pcaballero.ch@gmail.com>
2015-05-02 13:02:14 +0200

    heroku Author

--- models/models.js
+++ models/models.js
@@ -22,10 +22,15 @@ exports.Quiz=Quiz; //Exporta definicion de tabla Quiz
 
 //sequelize.sync() crea e inicializa tabla de preguntas en DB
 sequelize.sync().success(function(){
+
+	Quiz.count().success(function(count){
+
 	if(count===0){ //la tabla se inicializa solo si está vacia
 		Quiz.create({pregunta:'Capital de Italia',
 					respuesta:'Roma'
 					})
 		.success(function(){console.log('Base de datos inicializada')});
 	};
+
+	});
 });
