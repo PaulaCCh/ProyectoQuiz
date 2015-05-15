e506255232395d52262f091d3613e6d43d130320
PaulaCCh <pcaballero.ch@gmail.com>
2015-05-15 21:59:34 +0200

    Autorización

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -1,3 +1,11 @@
+// MW de autorización de accesos HTTP restringidos
+exports.loginRequired = function(req, res, next){
+    if (req.session.user) {
+        next();
+    } else {
+        res.redirect('/login'); // si no estas logeado te redirige a login
+    }
+};
 // Get /login   -- Formulario de login
 exports.new = function(req, res) {
     var errors = req.session.errors || {};
